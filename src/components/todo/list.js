91866beb9a143ea296d3b1d7308befe3e91703cf
Micha Davis <m.leigh.davis@gmail.com>
2022-05-02 21:09:49 -0700

    remove context and state from list

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,21 +1,17 @@
-import { useContext, useState } from 'react';
-import { SettingsContext } from '../../context/settings';
-
-
 const List = (props) => {
-  const settings = useContext(SettingsContext);
+
+
   return (
     props.list.map((item, index) => {
-      if (index < settings.numToDisplay)
-        return (
-          <div key={item.id}>
-            <p>{item.text}</p>
-            <p><small>Assigned to: {item.assignee}</small></p>
-            <p><small>Difficulty: {item.difficulty}</small></p>
-            <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-            <hr />
-          </div>
-        )
+      return (
+        <div key={item.id}>
+          <p>{item.text}</p>
+          <p><small>Assigned to: {item.assignee}</small></p>
+          <p><small>Difficulty: {item.difficulty}</small></p>
+          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+          <hr />
+        </div>
+      )
     })
   );
 }
