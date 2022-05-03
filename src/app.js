3ed4e1c9206e6c9ee0571c95e83c91224f0224c8
Micha Davis <m.leigh.davis@gmail.com>
2022-05-02 17:47:42 -0700

    move app logic and state out of todo and into app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useForm from './hooks/form.js';
+import Header from './components/header/header';
+import Form from './components/todo/form';
+import List from './components/todo/list';
 
-import ToDo from './components/todo/todo.js';
+import { v4 as uuid } from 'uuid'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <ToDo />
-    );
+function App() {
+
+  const defaultValues = {
+    difficulty: 4,
   }
+
+  const [list, setList] = useState([]);
+  const [incomplete, setIncomplete] = useState([]);
+  const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
+
+  function addItem(item) {
+    console.log(item);
+    item.id = uuid();
+    item.complete = false;
+    setList([...list, item]);
+  }
+
+  function deleteItem(id) {
+    const items = list.filter(item => item.id !== id);
+    setList(items);
+  }
+
+  function toggleComplete(id) {
+
+    const items = list.map(item => {
+      if (item.id == id) {
+        item.complete = !item.complete;
+      }
+      return item;
+    });
+
+    setList(items);
+
+    useEffect(() => {
+      let incompleteCount = list.filter(item => !item.complete).length;
+      setIncomplete(incompleteCount);
+      document.title = `To Do List: ${incomplete}`;
+    }, [list]);
+  }
+
+  return (
+    <>
+      <Header incomplete={incomplete} />
+
+      <Form handleChange={handleChange} handleSubmit={handleSubmit} defaultValues={defaultValues} />
+
+      <List list={list} />
+    </>
+  );
 }
+
+export default App;
